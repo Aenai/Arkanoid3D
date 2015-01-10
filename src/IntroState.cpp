68ff045cf6455df95c10dff6ede4b04ee89814cb
Aenai <ivan_mh@hotmail.es>
2015-01-10 21:48:32 +0100

    smooth movement of the paddle from the keyboard

--- src/IntroState.cpp
+++ src/IntroState.cpp
@@ -3,100 +3,80 @@
 
 template<> IntroState* Ogre::Singleton<IntroState>::msSingleton = 0;
 
-void
-IntroState::enter ()
+void IntroState::enter ()
 {
-  _root = Ogre::Root::getSingletonPtr();
+	_root = Ogre::Root::getSingletonPtr();
 
-  _sceneMgr = _root->createSceneManager(Ogre::ST_GENERIC, "SceneManager");
-  _camera = _sceneMgr->createCamera("IntroCamera");
-  _viewport = _root->getAutoCreatedWindow()->addViewport(_camera);
-  _viewport->setBackgroundColour(Ogre::ColourValue(1.0, 1.0, 1.0));
+	_sceneMgr = _root->createSceneManager(Ogre::ST_GENERIC, "SceneManager");
+	_camera = _sceneMgr->createCamera("IntroCamera");
+	_viewport = _root->getAutoCreatedWindow()->addViewport(_camera);
+	_viewport->setBackgroundColour(Ogre::ColourValue(1.0, 1.0, 1.0));
 
-  _exitGame = false;
+	_exitGame = false;
 }
 
-void
-IntroState::exit()
+void IntroState::exit()
 {
-  _sceneMgr->clearScene();
-  _root->getAutoCreatedWindow()->removeAllViewports();
+	_sceneMgr->clearScene();
+	_root->getAutoCreatedWindow()->removeAllViewports();
 }
 
-void
-IntroState::pause ()
+void IntroState::pause ()
 {
 }
 
-void
-IntroState::resume ()
+void IntroState::resume ()
 {
 }
 
-bool
-IntroState::frameStarted
-(const Ogre::FrameEvent& evt) 
+bool IntroState::frameStarted (const Ogre::FrameEvent& evt) 
 {
-  return true;
+	return true;
 }
 
-bool
-IntroState::frameEnded
-(const Ogre::FrameEvent& evt)
+bool IntroState::frameEnded (const Ogre::FrameEvent& evt)
 {
-  if (_exitGame)
-    return false;
-  
-  return true;
+	if (_exitGame)
+		return false;
+
+	return true;
 }
 
-void
-IntroState::keyPressed
-(const OIS::KeyEvent &e)
+void IntroState::keyPressed (const OIS::KeyEvent &e)
 {
-  // Transición al siguiente estado.
-  // Espacio --> PlayState
-  if (e.key == OIS::KC_SPACE) {
-    changeState(PlayState::getSingletonPtr());
-  }
+	// Transición al siguiente estado.
+	// Espacio --> PlayState
+	if (e.key == OIS::KC_SPACE) {
+		changeState(PlayState::getSingletonPtr());
+	}
 }
 
-void
-IntroState::keyReleased
-(const OIS::KeyEvent &e )
+void IntroState::keyReleased (const OIS::KeyEvent &e )
 {
-  if (e.key == OIS::KC_ESCAPE) {
-    _exitGame = true;
-  }
+	if (e.key == OIS::KC_ESCAPE) {
+		_exitGame = true;
+	}
 }
 
-void
-IntroState::mouseMoved
-(const OIS::MouseEvent &e)
+void IntroState::mouseMoved (const OIS::MouseEvent &e)
 {
 }
 
-void
-IntroState::mousePressed
-(const OIS::MouseEvent &e, OIS::MouseButtonID id)
+void IntroState::mousePressed (const OIS::MouseEvent &e, OIS::MouseButtonID id)
 {
 }
 
-void
-IntroState::mouseReleased
-(const OIS::MouseEvent &e, OIS::MouseButtonID id)
+void IntroState::mouseReleased (const OIS::MouseEvent &e, OIS::MouseButtonID id)
 {
 }
 
-IntroState*
-IntroState::getSingletonPtr ()
+IntroState* IntroState::getSingletonPtr ()
 {
-return msSingleton;
+	return msSingleton;
 }
 
-IntroState&
-IntroState::getSingleton ()
+IntroState& IntroState::getSingleton ()
 { 
-  assert(msSingleton);
-  return *msSingleton;
+	assert(msSingleton);
+	return *msSingleton;
 }
